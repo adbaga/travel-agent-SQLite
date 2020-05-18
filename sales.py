9b58ea69035608cca702c7f60e4d3dda796cbccb
adbaga <adbaga@taltech.ee>
2020-05-19 00:06:02 +0200

    Adding class and package for tabel, implememt functions, changed database to memory

--- sales.py
+++ sales.py
@@ -1,10 +1,10 @@
 import sqlite3
-from travel import Travel
+from travel import Travel, Package
 
-conn = sqlite3.connect('travel.db')
+conn = sqlite3.connect(':memory:')
 
 c = conn.cursor()
-
+c.execute("PRAGMA table_info(table_name)")
 
 c.execute(""" CREATE TABLE IF NOT EXISTS travel (
             ID INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,10 +13,61 @@ c.execute(""" CREATE TABLE IF NOT EXISTS travel (
             duration integer,
             package integer,
             price integer,
-            available text
+            available text,
+            FOREIGN KEY(package)
+            REFERENCES package
             ) """)
 
 conn.commit()
+
+c.execute(""" CREATE TABLE IF NOT EXISTS Package (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            package text,
+            hotelStar integer,
+            localTrans integer,
+            guide integer
+
+            ) """)
+
+conn.commit()
+
+
+def add_trip(trip):
+    with conn:
+        c.execute("INSERT OR REPLACE INTO travel (tripName, dest, duration, package, price, available) VALUES (:tripName, :dest, :duration,:package, :price, :available)", {
+        'tripName':trip.tripName, 'dest':trip.dest, 'duration':trip.duration, 
+        'package':trip.package, 'price':trip.price, 'available':trip.available})
+
+def add_package(package):
+    with conn:
+        c.execute("INSERT OR REPLACE INTO package (package, hotelStar, localTrans, guide) VALUES(:package, :hotelStar,:localTrans, :guide)", {
+        'package':package.package, 'hotelStar': package.hotelStar,
+        'localTrans': package.localTrans,'guide': package.guide } )
+
+def get_all_trip():
+    c.execute("Select * from Travel")
+    return c.fetchall()
+
+def get_package_details():
+    c.execute("Select * from package")
+    return c.fetchall()
+
+def get_trip_by_dest(dest):
+    c.execute("Select * from Travel WHERE dest =:dest", {'dest': dest})
+    return c.fetchall()
+
+def update_price(trip, price):
+    with conn:
+        c.execute("""UPDATE travel SET price = :price where dest = :dest and package =:package""",
+         {'dest': trip.dest, 'package': trip.package, 'price': price })
+
+def delete_trip(dest, package):
+    with conn:
+        c.execute("""DELETE from travel where dest = :dest and package =:package""",
+         {'dest': dest, 'package': package})
+        
+
+
 
 
 
@@ -31,19 +82,55 @@ trip_8 = Travel('Rwanda', 5, 'Basic', 800, 0)
 trip_9 = Travel('Kenya', 7, 'Exclusive', 1600, 1)
 trip_10 = Travel('Rusia', 6, 'Basic', 500, 1)
 
+package_1 = Package('Basic', 1, 0, 0)
+package_2 = Package('Deluxe', 2, 1, 0)
+package_3 = Package('Exclusive', 4, 1, 1)
 
-c.execute("INSERT OR REPLACE INTO travel (tripName, dest, duration, package, price, available) VALUES (:tripName, :dest, :duration,:package, :price, :available)", {
-'tripName':trip_1.tripName, 'dest':trip_1.dest, 'duration':trip_1.duration, 
-'package':trip_1.package, 'price':trip_1.price, 'available':trip_1.available})
-conn.commit()
 
-c.execute("Select * from Travel")
-conn.commit()
-print(c.fetchall())
+add_trip(trip_1)
+add_trip(trip_2)
+add_trip(trip_3)
+add_trip(trip_4)
+add_trip(trip_5)
+add_trip(trip_6)
+add_trip(trip_7)
+add_trip(trip_8)
+add_trip(trip_9)
+add_trip(trip_10)
 
-conn.close()
+add_package(package_1)
+add_package(package_2)
+add_package(package_3)
+
+
+
+
+#print(get_trip_by_dest('Stockholm'))
+
+#delete_trip('Stockholm','Basic')
+
+#print(get_all_trip())
+
+print(get_package_details())
+
+
 
 
 
 #dest, duration, package, price, available
 
+# c.execute("INSERT OR REPLACE INTO travel (tripName, dest, duration, package, price, available) VALUES (:tripName, :dest, :duration,:package, :price, :available)", {
+# 'tripName':trip_1.tripName, 'dest':trip_1.dest, 'duration':trip_1.duration, 
+# 'package':trip_1.package, 'price':trip_1.price, 'available':trip_1.available})
+# conn.commit()
+
+# c.execute("Select * from Travel")
+# conn.commit()
+# print(c.fetchall())
+
+
+# c.execute("Select * from Package")
+# conn.commit()
+# print(c.fetchall())
+
+conn.close()
--- travel.py
+++ travel.py
@@ -1,20 +1,25 @@
 class Travel:
-    """A sample Employee class"""
+
 
     def __init__(self, dest, duration, package, price, available):
         self.dest = dest
         self.duration = duration
         self.package = package
         self.price = price
-        self.available = available
-        
-        
+        self.available = available       
 
 
     @property
     def tripName(self):
-        return '{}.{}D'.format(self.dest, self.duration)
+        return '{}-{}D'.format(self.dest, self.duration)
 
 
     def __repr__(self):
         return "Travel('{} Days', '{}', {})".format(self.tripName, self.package, self.price)
+
+class Package:
+    def __init__(self, package, hotelStar, localTrans, guide):
+        self.package = package
+        self.hotelStar = hotelStar
+        self.localTrans = localTrans
+        self.guide = guide
